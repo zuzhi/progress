7089375213e6195d8051cc3b0bc151fa50100fb1
zuzhi <zuzhi.hu@gmail.com>
2024-08-19 23:43:17 +0800

    extract HowToUse

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import { updateTopic } from './reducers/topicReducer'
 import { setSession } from './reducers/sessionReducer'
 import EditorForm from './components/EditorForm'
 import Footer from './components/Footer'
+import HowToUse from './components/HowToUse'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -146,32 +147,7 @@ function App() {
           appearance={{ theme: ThemeSupa }}
           providers={[]}
         />
-        <div className='howtouse'>
-          <h3>
-            how to use <span className='product-name'>progress</span>?
-          </h3>
-          <ul>
-            <li>register</li>
-            <li>add a project</li>
-            <li>add some topics</li>
-            <li>update topic status</li>
-          </ul>
-          <p>then the project progress will automatically be calculated, yeah, that&apos;s the whole point.</p>
-          <p>
-            for example:
-          </p>
-          <ul>
-            <li>
-              <b>fullstackopen - 50%</b>
-              <ul>
-                <li><s>Part 0: Fundamentals of Web apps</s></li>
-                <li><s>Part 1: Introduction to React</s></li>
-                <li>Part 2: Communicating with server</li>
-                <li>Part 3: Programming a server with NodeJS and Express</li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+        <HowToUse />
       </div>
     )
   }
---
+++ src/components/HowToUse.jsx
@@ -0,0 +1,32 @@
+const HowToUse = () => {
+  return (
+    <div className='howtouse'>
+      <h3>
+        how to use <span className='product-name'>progress</span>?
+      </h3>
+      <ul>
+        <li>register</li>
+        <li>add a project</li>
+        <li>add some topics</li>
+        <li>update topic status</li>
+      </ul>
+      <p>then the project progress will automatically be calculated, yeah, that&apos;s the whole point.</p>
+      <p>
+        for example:
+      </p>
+      <ul>
+        <li>
+          <b>fullstackopen - 50%</b>
+          <ul>
+            <li><s>Part 0: Fundamentals of Web apps</s></li>
+            <li><s>Part 1: Introduction to React</s></li>
+            <li>Part 2: Communicating with server</li>
+            <li>Part 3: Programming a server with NodeJS and Express</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default HowToUse
